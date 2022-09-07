d12bfd1fdfb44a1ab695f98776f7661170a3fe27
Alexander Shpin <@lexhpin@gmail.com>
2022-09-07 15:26:22 -0500

    Initial Commit

---
+++ CryptoCurrencies/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CryptoCurrencies
+//
+//  Created by Alexander Shpin on 07.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
